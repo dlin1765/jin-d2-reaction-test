74580d91fb4ae4ad8b393d09020ff9ee4cb7daf6
dlin1765 <dannylin1765@gmail.com>
2024-11-25 14:04:01 -0800

    update text color css so it doesn't become white in some browsers

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,8 +12,8 @@ function Header(){
                 }}>
 
                 <div style={{display:"flex", flexGrow: 0, flexShrink: 1, flexBasis: '65%', alignItems:"center", justifyContent:"space-between"}}>
-                        <p style = {{fontSize: '18px', fontWeight: '400', lineHeight: .5, margin: 0}}>Jin d2 Reaction Test</p>
-                        <button style = {{fontSize: '18px', fontWeight: '400'}}>Register</button>
+                        <p style = {{fontSize: '18px', fontWeight: '400', lineHeight: .5, margin: 0, color: 'black'}}>Jin d2 Reaction Test</p>
+                        <button style = {{fontSize: '18px', fontWeight: '400', color: 'black'}}>Register</button>
                 </div>
             </div>
         
